540e9f2e39d89747bca14968bf4aa2d688a3589c
giulio7771 <giulio7771@gmail.com>
2019-05-12 04:32:53 -0300

    up

--- funcao.js
+++ funcao.js
@@ -158,3 +158,148 @@ function B(){
 		}
 	})
 }
+
+function C(){
+	var nome = document.getElementById("c-nome").value;
+	var idade = document.getElementById("c-idade").value;
+	var salario = document.getElementById("c-salario").value;
+	let rota = rota_raiz + "/create";
+	console.log("C rota:"+rota);
+
+	$.ajax({
+		url: rota,
+		type: 'POST',
+		dataType:'JSON',
+		data:'{"name":"'+nome+'","salary":"'+idade+'","age":"'+salario+'"}',
+		success: function (response){
+			//let person = JSON.parse(response);
+			person = response;
+			console.log("response:"+JSON.stringify(person));
+			putEmployee(person);
+		},
+		error: function(msgerror){
+			console.log(msgerror)
+		}
+	})
+
+
+}
+
+function D(){
+	var nome = document.getElementById("d-nome").value;
+	var idade = document.getElementById("d-idade").value;
+	var salario = document.getElementById("d-salario").value;
+	var id = document.getElementById("d-id").value;
+	let rota = rota_raiz + "/update/"+id;
+	$.ajax({
+		url: rota,
+		type: 'PUT',
+		dataType:'JSON',
+		data:'{"name":"'+nome+'","salary":"'+salario+'","age":"'+idade+'"}',
+		success: function (response){
+			console.log(response)
+			putEmployee(JSON.parse(response));
+		},
+		error: function(msgerror){
+			console.log(msgerror)
+		}
+	})
+
+}
+
+function E(){
+	var id = document.getElementById("e-id").value;
+	let rota = rota_raiz + "/delete/" + id; 
+	$.ajax({
+		url: rota,
+		type: 'DELETE',
+		dataType:'JSON',
+		success: function (response){
+			console.log("E response: "+response)
+			
+		},
+		error: function(msgerror){
+			console.log(msgerror)
+		}
+	})
+
+
+
+}
+
+function F(){
+	var name = document.getElementById("f-name").value;
+	var person = null;
+	person = getByNameG(name);
+	console.log("F person get by name:::::::"+person);
+	if(person != null){
+		if(confirm("Deseja excluir?: "+person['employee_name'])){
+			let id = person[id];
+			let rota = rota_raiz + "/delete/" + id; 
+			$.ajax({
+				url: rota,
+				type: 'DELETE',
+				dataType:'JSON',
+				success: function (response){
+					console.log("E response: "+response)
+					
+				},
+				error: function(msgerror){
+					console.log(msgerror)
+				}
+			})
+		
+		}
+
+	}
+
+}
+function getByNameG(name){
+$.ajax({
+		url: rota_raiz + '/employees',
+		type: 'GET',
+		success: function (response){
+			var result = JSON.parse(response);
+			for (var i of result){
+				if(i['employee_name'] == name){
+					return i;
+					
+				}
+			}
+		},
+		error: function(msgerror){
+			console.log(msgerror)
+		}
+	})
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
